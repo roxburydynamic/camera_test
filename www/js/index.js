95b14751ec433cb3796af3b5c2e987d1cec15acb
roxburydynamic <johnr@roxburydynamic.com>
2015-07-21 15:16:55 +0100

    add geo

--- www/js/index.js
+++ www/js/index.js
@@ -60,3 +60,16 @@ var app = {
       });
     }
 };
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    function disp(pos) {
+        $('.lat-view').html(pos.coords.latitude);
+        $('.long-view').html(pos.coords.longitude);
+    }
+    $('#getIt').click(function() {
+        navigator.geolocation.getCurrentPosition(disp);
+    });
+}
