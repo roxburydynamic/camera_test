30916bd0c1ff8f25d283883bd99a4d51dd0027bc
roxburydynamic <johnr@roxburydynamic.com>
2015-07-21 11:57:03 +0100

    added filesystem

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,30 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
+
     // Application Constructor
+
     initialize: function() {
         this.bindEvents();
     },
+
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -13,6 +34,8 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
 
+
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
@@ -20,7 +43,10 @@ var app = {
 
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        window.requestFileSystem(LocalFileSystem.PERSISTENT,0,onFSSuccess,onError);
     },
+
+
 
     // Update DOM on a Received Event
 
@@ -39,17 +65,17 @@ var app = {
       navigator.camera.getPicture( function( imageURI ) {
         alert( imageURI );
       },
-
       function( message ) {
         alert( message );
       },
-
       {
         quality: 50,
-        destinationType: Camera.DestinationType.FILE_URI, 
-        saveToPhotoAlbum: true,
-        targetWidth: 100,
-        targetHeight: 100
+        destinationType: Camera.DestinationType.FILE_URI
       });
     }
 };
+
+function onFSSuccess(fs) {
+  fileSystem=fs;
+  alert("got file system");
+}
