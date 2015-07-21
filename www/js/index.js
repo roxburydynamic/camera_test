d8cd6e82adb95f17d67b1aff8056a71af9f1ebb3
roxburydynamic <johnr@roxburydynamic.com>
2015-07-21 14:34:51 +0100

    changes

--- www/js/index.js
+++ www/js/index.js
@@ -17,39 +17,25 @@
  * under the License.
  */
 var app = {
-
     // Application Constructor
-
     initialize: function() {
         this.bindEvents();
     },
-
-
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-
-
-
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-
     },
-
-
-
     // Update DOM on a Received Event
-
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -74,4 +60,3 @@ var app = {
       });
     }
 };
-
